ec667969b90aeea671f67f58a897349b9e3f0eae
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-04 14:20:32 -0300

    inputprocessing file

--- forms.php
+++ forms.php
@@ -1,3 +1,8 @@
 <?php
 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
 
+
+echo "<h2>Formulário enviado, ". $nome . "!</h2>";
+echo "<h4> Confira a caixa de entrada no seu email em " . $email . "</h4>";
