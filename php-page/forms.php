7683a3c82e4657f9751360a1e633c95176beb10b
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-04 20:46:02 -0300

    change form path directory

--- php-page/forms.php
+++ php-page/forms.php
@@ -1,8 +1,9 @@
 <?php
 
+echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-
 
 echo "<h2>Formulário enviado, ". $nome . "!</h2>";
 echo "<h4> Confira a caixa de entrada no seu email em " . $email . "</h4>";
