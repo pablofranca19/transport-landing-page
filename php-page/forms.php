82d55344d68ebb8e6ec0415adeeb22b57e0b2fc5
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-05 14:50:14 -0300

    done: link n8n to form submission

--- php-page/forms.php
+++ php-page/forms.php
@@ -5,7 +5,7 @@ echo "<link rel='stylesheet' type='text/css' href='form-style.css'>";
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
-$data = [
+$formData = [
     'nome' => $nome,
     'email' => $email
 ];
@@ -17,11 +17,17 @@ echo "</main>";
 
 try {
     $webhook = curl_init('http://n8n:5678/webhook/check-email');
-    curl_setopt($webhook, CURLOPT_POST, true);
-    curl_setopt($webhook, CURLOPT_CONNECTTIMEOUT, 3);
-    curl_setopt($webhook, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($webhook, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($webhook, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt_array($webhook, [
+        CURLOPT_POST => true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POSTFIELDS => http_build_query($formData),
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/x-www-form-urlencoded',
+            'Accept: application/json'
+        ]
+    ]);
+
     $response = curl_exec($webhook);
 } catch (Exception $e) {
     error_log("N8N webhook error: " . $e->getMessage() . "\n cURL error: " . curl_error($webhook));
