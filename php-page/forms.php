18cd7136383be0d2b4b55ffdd5125f50f4165908
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-04 21:07:25 -0300

    add main tag

--- php-page/forms.php
+++ php-page/forms.php
@@ -1,9 +1,11 @@
 <?php
 
-echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+echo "<link rel='stylesheet' type='text/css' href='form-style.css'>";
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
+echo "<main>";
 echo "<h2>Formulário enviado, ". $nome . "!</h2>";
-echo "<h4> Confira a caixa de entrada no seu email em " . $email . "</h4>";
+echo "<h4> Confira a caixa de entrada no seu email em " . $email . ", entraremos em contato." ."</h4>";
+echo "</main>";
