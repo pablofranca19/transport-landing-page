eec7b159385b0e2459df81c92752e25bbd7a1c03
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-07 21:20:37 -0300

    update: verify webhook response code before taking action

--- php-page/forms.php
+++ php-page/forms.php
@@ -2,21 +2,24 @@
 
 echo "<link rel='stylesheet' type='text/css' href='form-style.css'>";
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
+$nome = htmlspecialchars($_POST['nome']);
+$email = htmlspecialchars($_POST['email']);
 
 $formData = [
     'nome' => $nome,
     'email' => $email
 ];
 
-echo "<main>";
-echo "<h2>Formulário enviado, ". $nome . "!</h2>";
-echo "<h4> Confira a caixa de entrada no seu email em " . $email . ", entraremos em contato." ."</h4>";
-echo "</main>";
-
 try {
     $webhook = curl_init('http://n8n:5678/webhook/check-email');
+
+    $responsecode = trim(http_response_code(), characters: "int()");
+
+    if (curl_getinfo($webhook, CURLINFO_HTTP_CODE) != 200 || curl_getinfo($webhook, CURLINFO_HTTP_CODE) != 201) {
+        echo "<h2> Não foi possível enviar para o email " . $email . "</h2>";
+        echo "<p> Erro código " . var_dump($responsecode) . "</p>";
+        return;
+    }
 
     curl_setopt_array($webhook, [
         CURLOPT_POST => true,
@@ -29,7 +32,13 @@ try {
     ]);
 
     $response = curl_exec($webhook);
+
 } catch (Exception $e) {
-    error_log("N8N webhook error: " . $e->getMessage() . "\n cURL error: " . curl_error($webhook));
+    error_log("Webhook error: " . $e->getMessage() . "\n cURL error: " . curl_error($webhook));
 }
+
+echo "<main>";
+echo "<h2>Formulário enviado, ". $nome . "!</h2>";
+echo "<h4> Confira a caixa de entrada no seu email em " . $email . ", entraremos em contato." ."</h4>";
+echo "</main>";
 
