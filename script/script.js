bf90ede532beea85db9fe4449c7b8af40fae1386
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-15 15:28:42 -0300

    minor changes

--- script/script.js
+++ script/script.js
@@ -27,12 +27,12 @@ function verifyFields() {
 
 function scrollToSection(selector) {
   const el = document.querySelector(selector);
-  const headerHeight = document.querySelector('header').offsetHeight;
+  const headerHeight = document.querySelector("header").offsetHeight;
   const top = el.getBoundingClientRect().top + window.scrollY - headerHeight;
-  window.scrollTo({ top, behavior: 'smooth' });
+  window.scrollTo({ top, behavior: "smooth" });
 }
 
-document.querySelector('#inicio-botao').addEventListener("click", () => scrollToSection('#inicio'));
-document.querySelector('#quem-somos-botao').addEventListener("click", () => scrollToSection('#quem-somos'));
-document.querySelector('#servicos-botao').addEventListener("click", () => scrollToSection('#nossos-serviços'));
-document.querySelector('#contato-botao').addEventListener("click", () => scrollToSection('#fale-conosco'));
+document.querySelector('#inicio-botao').addEventListener("click", () => scrollToSection("#inicio"));
+document.querySelector('#quem-somos-botao').addEventListener("click", () => scrollToSection("#quem-somos"));
+document.querySelector('#servicos-botao').addEventListener("click", () => scrollToSection("#nossos-serviços"));
+document.querySelector('#contato-botao').addEventListener("click", () => scrollToSection("#fale-conosco"));
